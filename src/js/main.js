b5e4893f00a5f9b3ba8e70690ec026e35e3b61ba
alesukhin <alesukhin@museumplus.ru>
2021-05-04 09:46:43 +0300

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,15 @@
-import Swiper from 'swiper/bundle';
-import {gsap} from "../../node_modules/gsap/dist/gsap";
-import {ScrollTrigger} from "../../node_modules/gsap/ScrollTrigger";
+import Swiper from "swiper/bundle";
+import { gsap } from "../../node_modules/gsap/dist/gsap";
+import { ScrollTrigger } from "../../node_modules/gsap/ScrollTrigger";
 
-import LocomotiveScroll from '../../node_modules/locomotive-scroll/dist/locomotive-scroll';
+import LocomotiveScroll from "../../node_modules/locomotive-scroll/dist/locomotive-scroll";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const mainSlider = new Swiper('.swiper-container', {
-    effect: 'coverflow',
+document.addEventListener("DOMContentLoaded", () => {
+  const mainSlider = new Swiper(".swiper-container", {
+    effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
-    slidesPerView: 'auto',
+    slidesPerView: "auto",
     mousewheelControl: true,
     initialSlide: 2,
     coverflowEffect: {
@@ -25,69 +25,86 @@ document.addEventListener('DOMContentLoaded', () => {
       onlyInViewport: false,
     },
     mousewheel: {
-      sensitivity: 1
+      sensitivity: 1,
     },
-    slideToClickedSlide: true
-
+    slideToClickedSlide: true,
   });
 
-  const mainSliderFade = new Swiper('.swiper-container1', {
-    effect: 'fade',
+  const mainSliderFade = new Swiper(".swiper-container1", {
+    effect: "fade",
     fadeEffect: {
-      crossFade: true
+      crossFade: true,
     },
     autoplay: {
       waitForTransition: false,
       delay: 4500,
-      disableOnInteraction: false
+      disableOnInteraction: false,
     },
     speed: 3500,
     loop: true,
-    simulateTouch: false
-  })
+    simulateTouch: false,
+  });
 });
 
-
 (() => {
-  const menuButton = document.querySelector('.main-nav__button');
-  const menuList = document.querySelector('.main-nav__list');
+  const menuButton = document.querySelector(".main-nav__button");
+  const menuList = document.querySelector(".main-nav__list");
+  const body = document.querySelector("body");
+  const lang = document.querySelector(".lang");
 
   if (menuButton) {
-    menuButton.addEventListener('click', () => {
-    let expanded = menuButton.getAttribute('aria-expanded') === 'true';
-    menuButton.setAttribute('aria-expanded', !expanded);
-    menuButton.classList.toggle('main-nav__button--open');
-    menuList.classList.toggle('main-nav__list--open');
-    document.querySelector('body').classList.toggle('page__body--fade');
-    document.querySelector('.lang').classList.toggle('lang--active');
-  });
+    menuButton.addEventListener("click", () => {
+      let expanded = menuButton.getAttribute("aria-expanded") === "true";
+      menuButton.setAttribute("aria-expanded", !expanded);
+      menuButton.classList.toggle("main-nav__button--open");
+      menuList.classList.toggle("main-nav__list--open");
+      body.classList.toggle("page__body--fade");
+      lang.classList.toggle("lang--active");
+    });
+
+    document.onkeydown = function (evt) {
+      evt = evt || window.event;
+      if (evt.keyCode == 27) {
+        menuButton.classList.remove("main-nav__button--open");
+        menuList.classList.remove("main-nav__list--open");
+        body.classList.remove("page__body--fade");
+        lang.classList.remove("lang--active");
+      }
+    };
   }
 })();
 
-let dropdown = document.querySelectorAll('.main-nav__item--with-dropdown')
+let dropdown = document.querySelectorAll(".main-nav__item--with-dropdown");
 
-dropdown.forEach(element => element.addEventListener('click', event => {
-  // event.preventDefault();
-  // event.stopPropagation();
+dropdown.forEach((element) =>
+  element.addEventListener("click", (event) => {
+    // event.preventDefault();
+    // event.stopPropagation();
 
-  document.querySelectorAll('.main-nav__item--with-dropdown').forEach((element) => {
-   element.classList.remove('main-nav__item--open')
-  })
-  element.classList.add('main-nav__item--open')
-
-  let innerLists = element.querySelector('ul');
-  if (innerLists) {
-    if (!innerLists.classList.contains("main-nav__sublist--open")) {
-      document.querySelector("ul").querySelectorAll("ul").forEach(elm => {
-        if (!isDescendant(elm, element)) {
-          elm.classList.remove("main-nav__sublist--open")
-        }
+    document
+      .querySelectorAll(".main-nav__item--with-dropdown")
+      .forEach((element) => {
+        element.classList.remove("main-nav__item--open");
       });
-    }
+    element.classList.add("main-nav__item--open");
 
-    innerLists.classList.toggle("main-nav__sublist--open");
-  }
-}));
+    let innerLists = element.querySelector("ul");
+    if (innerLists) {
+      if (!innerLists.classList.contains("main-nav__sublist--open")) {
+        document
+          .querySelector("ul")
+          .querySelectorAll("ul")
+          .forEach((elm) => {
+            if (!isDescendant(elm, element)) {
+              elm.classList.remove("main-nav__sublist--open");
+            }
+          });
+      }
+
+      innerLists.classList.toggle("main-nav__sublist--open");
+    }
+  })
+);
 
 function isDescendant(parent, child) {
   let node = child.parentNode;
@@ -100,49 +117,43 @@ function isDescendant(parent, child) {
   return false;
 }
 
-if (document.querySelector('[data-scroll-container]')) {
+if (document.querySelector("[data-scroll-container]")) {
   const scroller = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true
-  })
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
+  });
 
+  gsap.registerPlugin(ScrollTrigger);
 
-gsap.registerPlugin(ScrollTrigger)
+  scroller.on("scroll", ScrollTrigger.update);
 
+  ScrollTrigger.scrollerProxy(".container", {
+    scrollTop(value) {
+      return arguments.length
+        ? scroller.scrollTo(value, 0, 0)
+        : scroller.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return {
+        left: 0,
+        top: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+  });
 
-scroller.on('scroll', ScrollTrigger.update)
+  ScrollTrigger.create({
+    trigger: ".section__image-mask",
+    scroller: ".container",
+    start: "top+=30% 50%",
+    end: "bottom-=40% 50%",
+    animation: gsap.to(".section__image-mask", { backgroundSize: "120%" }),
+    scrub: 5,
+    // markers: true
+  });
 
-ScrollTrigger.scrollerProxy(
-  '.container', {
-      scrollTop(value) {
-          return arguments.length ?
-          scroller.scrollTo(value, 0, 0) :
-          scroller.scroll.instance.scroll.y
-      },
-      getBoundingClientRect() {
-          return {
-              left: 0, top: 0,
-              width: window.innerWidth,
-              height: window.innerHeight
-          }
-      }
-  }
-)
+  ScrollTrigger.addEventListener("refresh", () => scroller.update());
 
-
-ScrollTrigger.create({
-  trigger: '.section__image-mask',
-  scroller: '.container',
-  start: 'top+=30% 50%',
-  end: 'bottom-=40% 50%',
-  animation: gsap.to('.section__image-mask', {backgroundSize: '120%'}),
-  scrub: 5,
-  // markers: true
-})
-
-
-ScrollTrigger.addEventListener('refresh', () => scroller.update())
-
-
-ScrollTrigger.refresh()
+  ScrollTrigger.refresh();
 }
