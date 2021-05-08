b40e032d681147b2d40345df8af40e58a346df3e
alesukhin <alesukhin@museumplus.ru>
2021-05-08 23:35:19 +0300

    Updates

--- js/openseadragon/hotspots.js
+++ js/openseadragon/hotspots.js
@@ -1,8 +1,8 @@
 var map = (function (me, undefined) {
   me.hotspots = [
     {
-      x: 4500,
-      y: 2500,
+      x: 500,
+      y: 500,
       w: 250,
       h: 250,
       id: "1",
@@ -11,11 +11,12 @@ var map = (function (me, undefined) {
       audio: "",
       // checkResize: false,
       category: "",
-      title: "Точка 1",
+      title: "Исследование нитей",
       description: `
       <div class="ssb__full-text">
-        <p>Макрофотография шерстяной красной нити при различном увеличении.
-        Шерстяная нить II порядка, толщиной ок 1,2 мм, S-крутки с шагом 4,17 мм., состоит из двух нитей I порядка толщиной ок. 0,68 мм, Z-крутки с шагом ок. 1,87 мм. </p>
+        <p>
+        -дипзум, при клике выводить окно с картинками попарно до\после. На пк просто рядом стоящие изображения, на мобилке и планшете через до\после
+        </p>
       </div>
 
       <!-- Изображение до/после на пк и планшете -->
@@ -37,7 +38,46 @@ var map = (function (me, undefined) {
         <img src="images/before-after/before-after-thumb-1b.jpg" />
       </div>
 			`,
-      data: "",
+      data: "research",
+    },
+    {
+      x: 4500,
+      y: 2500,
+      w: 250,
+      h: 250,
+      id: "1",
+      link: "1",
+      image: "",
+      audio: "",
+      // checkResize: false,
+      category: "",
+      title: "Это точка про птицу",
+      description: `
+      <div class="ssb__full-text">
+        <p>Очень много текста</p>
+        <p>при клике выводится окно с 2 изображениями, на пк просто рядом стоящие изображения, на мобилке и планшете через до-после</p>
+      </div>
+
+      <!-- Изображение до/после на пк и планшете -->
+      <div class="ssb__before-after">
+        <!-- Изображение до -->
+        <a class="ssb__gallery-popup-link" href="images/before-after/before-after-thumb-1.jpg">
+          <img src="images/before-after/before-after-thumb-1.jpg" />
+        </a>
+        <!-- Изображение после -->
+        <a class="ssb__gallery-popup-link" href="images/before-after/before-after-thumb-1b.jpg">
+          <img src="images/before-after/before-after-thumb-1b.jpg" />
+        </a>
+      </div>
+
+      <div class="before-after  ssb__before-after-slider">
+        <!-- Изображение до -->
+        <img src="images/before-after/before-after-thumb-1.jpg" />
+        <!-- Изображение после -->
+        <img src="images/before-after/before-after-thumb-1b.jpg" />
+      </div>
+			`,
+      data: "bird",
     },
     {
       x: 1500,
@@ -50,8 +90,13 @@ var map = (function (me, undefined) {
       audio: "",
       // checkResize: false,
       category: "",
-      title: "Мода",
+      title: "Растение",
       description: `
+      <div class="ssb__full-text">
+        <p>Дублирует раздел про птиц, отличается только цветом точки.</p>
+        <p>Очень много текста</p>
+        <p>при клике выводится окно с 2 изображениями, на пк просто рядом стоящие изображения, на мобилке и планшете через до-после</p>
+      </div>
       <div class="ssb__before-after">
         <!-- Изображение до -->
         <a class="ssb__gallery-popup-link" href="images/fashion-1.jpg">
@@ -66,7 +111,7 @@ var map = (function (me, undefined) {
         <img src="images/fashion-1b.jpg" />
       </div>
 			`,
-      data: "research",
+      data: "plants",
     },
     {
       x: 5500,
@@ -75,27 +120,19 @@ var map = (function (me, undefined) {
       h: 250,
       id: "2",
       link: "2",
-      image: "",
+      image: "images/intro-venera.jpg",
       audio: "",
       // checkResize: false,
       category: "",
-      title: "Точка 2",
+      title: "Сюжет",
       description: `
-      <div class="ssb__before-after">
-        <!-- Изображение до -->
-        <a class="ssb__gallery-popup-link" href="images/bird-1.jpg">
-          <img src="images/bird-1.jpg" />
-        </a>
-        <a class="ssb__gallery-popup-link" href="images/bird-1b.jpg">
-          <img src="images/bird-1b.jpg" />
-        </a>
-      </div>
-      <div class="before-after  ssb__before-after-slider">
-        <img src="images/bird-1.jpg" />
-        <img src="images/bird-1b.jpg" />
+      <div class="ssb__full-text">
+        <p>Обычный шаблон. В поле image добавляется картинка, ниже идет заголовок и текст</p>
+        <p>Очень много текста</p>
+        <p>при клике выводится окно, где есть фото и описание (см. триумф надежды)</p>
       </div>
 			`,
-      data: "bird",
+      data: "story",
     },
     {
       x: 1500,
@@ -108,10 +145,11 @@ var map = (function (me, undefined) {
       audio: "",
       // checkResize: false,
       category: "",
-      title: "Точка 3",
+      title: "Это просто точка, со всем содержимым",
       description: `
       <div class="ssb__full-text">
         <p>Сюда пишется весь текст</p>
+        <p>Тут может быть картинка, секция до-после, галерея с попапами</p>
       </div>
 
       <div class="ssb__gallery">
@@ -153,7 +191,7 @@ var map = (function (me, undefined) {
         </a>
       </div>
 			`,
-      data: "story",
+      data: "",
     },
     {
       x: 500,
@@ -166,11 +204,10 @@ var map = (function (me, undefined) {
       audio: "",
       // checkResize: false,
       category: "",
-      title: "Точка 1",
+      title: "Это точка мода",
       description: `
       <div class="ssb__full-text">
-        <p>Макрофотография шерстяной красной нити при различном увеличении.
-        Шерстяная нить II порядка, толщиной ок 1,2 мм, S-крутки с шагом 4,17 мм., состоит из двух нитей I порядка толщиной ок. 0,68 мм, Z-крутки с шагом ок. 1,87 мм. </p>
+        <p>Выводится картинка с подсказками и внизу галерея с похожими костюмами</p>
       </div>
       <div class="ssb__gallery">
         <a class="ssb__gallery-popup-link" href="images/before-after/before-after-thumb-1.jpg">
@@ -211,7 +248,7 @@ var map = (function (me, undefined) {
         </a>
       </div>
 			`,
-      data: "bird",
+      data: "fashion",
     },
   ];
 
